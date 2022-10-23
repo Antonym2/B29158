fd8244f252f460442056ab4fe362e42e66e6320f
Brux <timafey.bish@gmail.com>
2022-10-23 12:30:52 +0300

    первые переменные

---
+++ 2.py
@@ -0,0 +1,8 @@
+import random
+fp_score = 0
+fc_score = 0
+choices = ["камень", "бумага", "ножницы"]
+print("Камень > ножницы. Ножницы > бумагу. Бумага > камень.")
+while True:
+    p_score = 0
+    c_score = 0
