e7ed137b284f889c404f871abac17ef429f40036
Brux <timafey.bish@gmail.com>
2022-10-30 11:33:29 +0300

    init + готовая работа

--- 213.py
+++ 213.py
@@ -1,22 +1,36 @@
-q1 = int (input('Введите число 1: '))
-q2 = int (input('Введите число 2: '))
+f_num = int(input('Введите число 1: '))
+s_num = int(input('Введите число 2: '))
+while True:
+    print(f"(число 1) {f_num}")
+    print(f"(число 2) {s_num}")
+    num1 = 0
+    num2 = 0
+    result = 0
+    v = int(input('Какую операцию вы хотите выполнить? \n 1 Сложение \n 2 Вычитание \n 3 Деление \n 4 Умножение \n 5 Замена чисел \n 6 Выход \n'))
 
-v = int (input('Какую операцию вы хотите выполнить? \n 1 Сложение \n 2 Вычитание \n 3 Деление \n 4 Умножение \n'))
+    if v == 1:
+        result = f_num + s_num
+        print('Результат сложения = ', result)
+    if v == 2:
+        result = f_num - s_num
+        print('Результат вычитания = ', result)
+    if v == 3:
+        if s_num != 0:
+            result = float(f_num / s_num)
+            print('Результат деления = ', result)
+        else:
+            print("на ноль делить нельзя")
+            num2 = int(input("давайте заменим число (2) "))
+            s_num = num2
+    if v == 4:
+        result = f_num * s_num
+        print('Результат умножения = ', result)
+    if v == 5:
+        num1 = int(input("введите новое число (1) "))
+        num2 = int(input("введите новое число (2) "))
+        f_num = num1
+        s_num = num2
+    if v == 6:
+        print("надеюсь вам понравился мой калькулятор")
+        break
 
-if v == 1:
-    r = q1 + q2
-    p = 'сложения'
-    t = p
-if v == 2:
-    r = q1 - q2
-    l = 'вычитания'
-    t = l
-if v == 3:
-    r = float(q1 / q2)
-    m = 'деления'
-    t = m
-if v == 4:
-    r = q1 * q2
-    n = 'умножения'
-    t = n
-print ('Результат ',t,' = ',r)
